fe708725d7fc1ef4ab5ab9706b99d66a06bc28e9
hs lee <dlgudtn032@naver.com>
2020-12-14 15:41:24 +0900

    commit

---
+++ src/components/habbitAddForm.jsx
@@ -0,0 +1,32 @@
+import React, { memo } from 'react';
+
+
+
+const HabbitAddForm = memo(props => {
+    const inputRef = React.createRef();
+    const formRef = React.createRef();
+
+    const onSubmit = event => {
+        event.preventDefault();
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
+        // this.inputRef.current.value="";
+        formRef.current.reset();
+    };
+   
+        return (
+            <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+                <input 
+                ref={inputRef}
+                type="text" 
+                className="add-input" 
+                placeholder="Habit"/>
+                <button className="add-button">Add</button>
+            </form>
+        );
+})
+
+
+
+
+export default HabbitAddForm;
